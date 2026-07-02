6f21ba58a561db0e95d9c5f10ea316604d8b905f
FalishaRamadhan <Falisha.Ramadhan@strathmore.edu>
2026-07-02 11:01:39 +0300

    Update PasswordResetController.php

--- totobora/app/Http/Controllers/Auth/PasswordResetController.php
+++ totobora/app/Http/Controllers/Auth/PasswordResetController.php
@@ -32,7 +32,7 @@ class PasswordResetController extends Controller
             $request->only('email')
         );
 
-        return $status === Password::RESET_LINK_SENT
+        return $status === Password::RESET_LINK_HAS_BEEN_SENT
             ? back()->with('status', __($status))
             : back()->withErrors([
                 'email' => __($status),
